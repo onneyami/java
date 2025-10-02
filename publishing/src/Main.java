efb61b488d3e1d1758b908806e571b80f967d71c
onneyami <andreyonneyami@gmail.com>
2025-10-02 12:12:58 +0300

    Update Main.java

--- publishing/src/Main.java
+++ publishing/src/Main.java
@@ -2,7 +2,7 @@ class Publishing {
     public static void main(String[] args) {
         System.out.println("Authors:");
 
-        //создание экземпляра класса Author
+        // Creating an instance of the Author class
         Author vasilev = new Author(
                 "Vasilev", "Pavel", "Ivanovich",
                 "MP2035648, got 18.05.2011",
@@ -22,7 +22,7 @@ class Publishing {
 
         System.out.println("\nPublished materials:");
 
-        //создание двух экземпляров класса Note через конструктор с параметрами
+        // Creating two instances of the Note class using the constructor with parameters
         Note noteVasilev1 = new Note("Creating classes in Java", shortNameVasilev, 3.6);
         Note noteVasilev2 = new Note("NetBeans environment", shortNameVasilev, 2.8);
 
@@ -31,19 +31,19 @@ class Publishing {
         System.out.println(noteVasilev2.toString());
         System.out.println("Number of pages " + noteVasilev2.getNumberPages());
 
-        //вызов метода getLastNumberNote() для получения подсчета количества опубликованных материалов
+        // Calling getLastNumberNote() to get the count of published materials
         System.out.println("\nNumber of published materials:" + Note.getLastNumberNote());
 
-        //изменение автора и порядкового номера книги "Creating classes in Java"
-        //noteVasilev1.numberNote = 20;
-        //noteVasilev1.authorNote = "Petrov D.V.";
-        //System.out.println(noteVasilev1.toString());
+        // Changing the title and number of author sheets for the book "Creating classes in Java"
+        // noteVasilev1.numberNote = 20;
+        // noteVasilev1.authorNote = "Petrov D.V.";
+        // System.out.println(noteVasilev1.toString());
 
         noteVasilev1.setNoteTitle("Classes in Java");
         noteVasilev1.setNumberAuthorsSheets(3.5);
         System.out.println(noteVasilev1.toString());
 
-        //проверка работы сеттеров класса Author
+        // Testing the setters of the Author class
         vasilev.setPhoneNumber("8(029)44-564-78-02");
         System.out.println(vasilev.toString());
     }
